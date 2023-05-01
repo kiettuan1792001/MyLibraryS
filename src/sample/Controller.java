fdd9666d105d9216ec55199a2e9e4a43bb0550fa
kiettuan1792001 <kietuan2001@gmail.com>
2023-05-02 02:20:27 +0700

    init project

---
+++ src/sample/Controller.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class Controller {
+    public void onClick(){
+        System.out.println("Clicking !!!!");
+    }
+    public void close(){
+        System.exit(0);
+    }
+}
